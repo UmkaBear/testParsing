f91851dc4a5076307138b78899dabfe4f8a74b55
UmkaBear <dfakhiev@yandex.ru>
2025-02-13 15:49:16 +0500

    Получение данных с номером и ценой

--- parser.php
+++ parser.php
@@ -12,4 +12,14 @@ if (!$html) {
     die("Ошибка загрузки страницы");
 }
 
+preg_match('/<a href="\/epz\/order\/notice\/ea44\/view\/common-info\.html\?regNumber=(\d+)"/', $html, $matches);
+$auctionNumber = $matches[1] ?? 'Не найдено';
+
+
+preg_match('/<span class="cardMainInfo__content cost">\s*([\d\s,.]+)\s*[^0-9]/u', $html, $matches);
+$startPrice = trim(str_replace([' ', ' ', ','], ['', '', '.'], $matches[1] ?? '0'));
+
+
+echo "Номер аукциона: $auctionNumber\n";
+echo "Начальная цена: $startPrice руб.\n";
 
