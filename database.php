2b2e0a94ea2337335aa7ec567dc6b9e289de4c26
UmkaBear <dfakhiev@yandex.ru>
2025-02-13 15:09:32 +0500

    Создаем БД и подключаемся к ней

---
+++ database.php
@@ -0,0 +1,11 @@
+<?php
+$config = require 'config.php';
+
+try {
+    $pdo = new PDO("mysql:host={$config['db_host']};dbname={$config['db_name']}", $config['db_user'], $config['db_pass'], [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]);
+} catch (PDOException $e) {
+    die("Ошибка подключения к базе данных: " . $e->getMessage());
+}
